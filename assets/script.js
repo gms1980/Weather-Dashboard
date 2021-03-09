df7f11d9f2abea1761d579b403af3000dd5749e9
George <gms1980@icloud.com>
2021-03-09 14:27:36 -0700

    updated html

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,53 @@
-//class Fetch {
-//    async getCurrent(input) {
-      //const myKey = "6e87985c0e93d8622bf6dcb03fb22522";
+fetch(
+    'https://api.openweathermap.org/data/2.5/weather?api_key=6e87985c0e93d8622bf6dcb03fb22522https://api.openweathermap.org/data/2.5/onecall?api_key=6e87985c0e93d8622bf6dcb03fb22522'
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    });
+
+    
   
-      //make request to url
+// class Fetch {
+//     async getCurrent(input) {
+//       const myKey = "6e87985c0e93d8622bf6dcb03fb22522";
   
-       fetch(
-        "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6e87985c0e93d8622bf6dcb03fb22522"
-        )
+//       //make request to url
   
-      .then(function(response) {
-       return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-      });
+//       const response = await fetch(
+//         `https://api.openweathermap.org/data/2.5/weather?api_key=6e87985c0e93d8622bf6dcb03fb22522`
+//       );
+  
+//       const data = await response.json();
+  
+//       console.log(data);
+  
+//       return data;
+//     }
+//   }
+//   const ft = new Fetch();
+// const ui = new UI();
+
+// //add event listeners//
+
+// const search = document.getElementById("searchUser");
+// const button = document.getElementById("submit");
+// button.addEventListener("click", () => {
+//   const currentVal = search.value;
+
+//   ft.getCurrent(currentVal).then((data) => {
+//     //call a UI method//
+//     ui.populateUI(data);
+//     //call saveToLS
+//     ui.saveToLS(data);
+//   });
+// });
+
+// //event listener for local storage
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   const dataSaved = ui.getFromLS();
+//   ui.populateUI(dataSaved);
+// });
