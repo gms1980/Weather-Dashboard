a9fc9496dc68c7cace0fb6cd93b37ed947e50ce7
George <gms1980@icloud.com>
2021-03-11 19:30:59 -0700

    added handle API

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,29 @@
-fetch(
-    'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6e87985c0e93d8622bf6dcb03fb22522'
-    
-  )
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data);
-      var responseSubmitEl = document.querySelector('submit');
+    const submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", () => {
+      //console.log(clicked submitButton);
+    const searchCity = document.getElementById("searchCity").value;
+    console.log(searchCity);
+    handleAPI(searchCity);
+    });
 
+    //Function that handles API async and await to replace .then
+    
+    async function handleAPI(cityName) {
+      let response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=6e87985c0e93d8622bf6dcb03fb22522`
+        )
+        let data = await response.json()
+        console.log(data);
+        let uvResponse = await fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=6e87985c0e93d8622bf6dcb03fb22522`)
+        let uvData = await uvResponse.json()
+        console.log(uvData);        
+    }
+        
+       
+     
+    
+
+    //Display info on the board take info and display it
+    //function displayCityWeather () {}
+    
+    
